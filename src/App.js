3313d48fb1eb29de6f42389d237ccc57fd84b058
Bartek Lopatka <bartek.lopatka@mbp-bartek.home>
2019-08-09 09:54:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. You are in the matrix?
         </p>
         <a
           className="App-link"
